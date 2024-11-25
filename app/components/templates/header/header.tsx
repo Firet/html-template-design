009e13ff7f571fed89043d24358835eda090935c
Firet <damianfederico@protonmail.com>
2024-11-25 19:33:52 -0300

    Add cart icon

--- app/components/templates/header/header.tsx
+++ app/components/templates/header/header.tsx
@@ -24,8 +24,8 @@ export default function Header() {
         <div className="nav-item">Sell<img src="./arrow-down.svg" className="arrow" width={11} alt="arrow down" /></div>
       </section>
       <section className="user-section">
-        <div>USD</div>
-        <div>CART(5)</div>
+        <div className="nav-item">USD <img src="./arrow-down.svg" className="arrow" width={11} alt="arrow down" /></div>
+        <div className="nav-item"> <img src="./cart.svg" className="cart" width={11} alt="cart" />CART(5)</div>
         <div><Button priority="accent">SIGN IN</Button></div>
       </section>
     </header>
