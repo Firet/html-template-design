4897b4a2da2e12d4ade107d04e7716eb35514f7e
Firet <damianfederico@protonmail.com>
2024-11-25 20:26:49 -0300

    Add mobile responsiveness to user item in header

--- app/components/templates/header/header.tsx
+++ app/components/templates/header/header.tsx
@@ -24,9 +24,9 @@ export default function Header() {
         <div className="nav-item">Sell<img src="./arrow-down.svg" className="arrow" width={11} alt="arrow down" /></div>
       </section>
       <section className="user-section">
-        <div className="nav-item">USD <img src="./arrow-down.svg" className="arrow" width={11} alt="arrow down" /></div>
-        <div className="nav-item"> <img src="./cart.svg" className="cart" width={11} alt="cart" />CART(5)</div>
-        <div><Button priority="accent">SIGN IN</Button></div>
+        <div className="user-item">USD <img src="./arrow-down.svg" className="arrow" width={11} alt="arrow down" /></div>
+        <div className="user-item"> <img src="./cart.svg" className="cart" width={11} alt="cart" />CART(5)</div>
+        <div className="user-item"><Button priority="accent">SIGN IN</Button></div>
       </section>
     </header>
   );
